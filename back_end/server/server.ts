792f4cfe4d646a7b0055ce62fe48612bc3233218
Ubuntu <azureuser@grupo6sm.4com11oydy2e3bp1dp3yykww1c.cx.internal.cloudapp.net>
2026-04-26 21:52:31 +0000

    docs: adiciona documentação Swagger

--- back_end/server/server.ts
+++ back_end/server/server.ts
@@ -7,20 +7,21 @@ import doacaoRouter from "./src/routes/doacao.routes";
 import localidadeRouter from "./config/routes/localidade.router";
 import cors from "cors";
 import chatRouter from "./src/routes/chat.routes";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 dotenv.config({ path: "../.env" });
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors()); // libera qualquer origem
 
 // Express routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", usuarioRouter);
 app.use("/api", alimentoRouter);
 app.use("/api", campanhaRouter);
@@ -28,6 +29,6 @@ app.use("/api", doacaoRouter);
 app.use("/api", localidadeRouter);
 app.use("/api", chatRouter);
 
-app.listen(5000, () => {
+app.listen(5000, "0.0.0.0", () => {
   console.log("Server started on port 5000");
 });
